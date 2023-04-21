ea73618174800006985028817b349c1dc14c9ff2
Shavarsh Abrahamyan <shavarsh.abrahamyan1993@gmail.com>
2023-04-21 12:07:35 +0400

    added prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import {typeDefs} from "./schema";
 
 const toDos = [];
 
@@ -9,6 +8,30 @@ const resolvers = {
         toDos: () => toDos,
     },
 };
+const typeDefs = `    #graphql
+# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+# This "tod" type defines the queryable fields for every book in our data source.
+type ToDo {
+    id: ID!
+    title: String!
+    createdAt: String!
+    updatedAt: String!
+}
+
+# The "Query" type is special: it lists all of the available queries that
+# clients can execute, along with the return type for each. In this
+# case, the "todos" query returns an array of zero or more ToDos (defined above).
+type Query {
+    toDos: [ToDo]
+}
+
+type Mutation {
+    addToDo(id: ID, title: String): ToDo
+    editToDo(id: ID, title: String): ToDo
+}
+
+`;
 
 const server = new ApolloServer({
     typeDefs,
